514c692988aee6ffbb35d9fa3658dbbcd2eee4de
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-04-08 18:50:14 +0300

    доробив

--- js/index3.js
+++ js/index3.js
@@ -4,10 +4,24 @@ const x1 = 10;
 
  if (number < x1) {
     console.log(`${number} входить до ${x1} `);
- } else if (number > x1 ) {
-    console.log(`${number} не входить до ${x1}`);
- } else if (x1 < number  < x2) {
-    console.log(`${number} не входить в ${x1} та входить в ${x2}`);
- } else if (number < x1 || number > x2) {
-    console.log(`${number} входить до ${x1} або ${number} не входить ${x1}`);
+ } else {
+   console.log(`${number} не входить до ${x1} `);
  };
+
+ if (number > x1 ) {
+   console.log(`${number} входить до ${x1}`);
+} else {
+   console.log(`${number} не входить до ${x1}`);
+};
+
+if (x1 < number  < x2) {
+   console.log(`${number} входить в ${x1} та входить в ${x2}`);
+} else {
+   console.log(`${number} не входить в ${x1} та не входить в ${x2}`);
+};
+
+if (number < x1 || number > x2) {
+   console.log(`${number} входить до ${x1} або ${number} не входить ${x1}`);
+} else {
+   console.log(`${number} не входить до ${x1} або ${number}  входить ${x1}`);
+}
